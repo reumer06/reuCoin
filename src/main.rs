515f91b5e81af3010c155fb5938f779eba637a55
n0sync <n0sync.76@gmail.com>
2026-01-05 16:51:45 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+// fn main() {
+//     'bitcoin_lifetime: {
+//     let mut bitcoin = String::from("Hello Bitcoin");
+//     'mut_ref_lifetime : {
+//     let mut_ref = &mut bitcoin;
+//     'double_ref_lifetime : {
+//         let double_ref = &bitcoin;
+//
+//         println!("{}", double_ref);
+//
+//         mut_ref.push_str(" the cryptocurrency");
+//     }}}
+//
+//     // println!("{}",bitcoin);
+// }
+
+
+// https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html
+
+
+fn main()
+{
+    
+}
