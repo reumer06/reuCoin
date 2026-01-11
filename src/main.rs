734578f9f44e9a8c927c32247ee6053477e52574
n0sync <n0sync.76@gmail.com>
2026-01-12 02:19:04 +0530

    feat: Gambler ruins problem implementation
    p = probability an honest node finds the next block
    q = probability the attacker finds the next block
    qz = probability the attacker will never catch up from z block behind
    
    q = {1 , if p<=q
        (p/q)^z  if p>q}

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
-// fn main() {
-//     'bitcoin_lifetime: {
-//     let mut bitcoin = String::from("Hello Bitcoin");
-//     'mut_ref_lifetime : {
-//     let mut_ref = &mut bitcoin;
-//     'double_ref_lifetime : {
-//         let double_ref = &bitcoin;
-//
-//         println!("{}", double_ref);
-//
-//         mut_ref.push_str(" the cryptocurrency");
-//     }}}
-//
-//     // println!("{}",bitcoin);
-// }
+use std::f64::consts::E; // Binomial Random Walk
 
+fn attacker_success_possibility(q: f64,z : i32) -> f64{
+    let p = 1.0 - q;
+    let lamda =  z as f64 * (q/p);
+    let mut sum = 1.0;
+    for k in 0..=z {
+        let mut poisson  = E.powf(-lamda);
+        for i in 1..=k {
+            poisson *=lamda / i as f64;
+        }
+        sum -= poisson * (1.0 - (q/p).powi(z - k));
+    }
+    sum
+}
 
-// https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html
-
-
-fn main()
-{
-    
+fn main() {
+    attacker_success_possibility(67.69,12);
 }
