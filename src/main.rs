54d7f5ff9f9855233d6c6ecbf1238f1c87512404
n0sync <n0sync.76@gmail.com>
2026-01-22 01:39:05 +0530

    --understating lifetime annotations

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,13 @@
-use std::env;
-use std::process::exit;
 
-fn main(){
-    let args: Vec<String>  = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} <op> <text>",args[0]);
-        exit(1);
+fn max_ref<'qa>(left: &'qa i32,right: &'qa i32) -> &'qa i32 { // lifetime annotations
+    if *left < *right{
+        right
+    } else {
+        left
     }
-    let op = &args[1];
-    let text = &args[2];
+}
+fn main(){
 
-    println!("op : {op}");
-    println!("text: {text}");
-     
-    let res = match op.as_str() {
-        "reverse" => text.chars().rev().collect::<String>(),
-        "uppercase" => text.to_uppercase(),
-        "invert" => text 
-            .chars()
-            .map(|c| {
-                if c.is_uppercase() {
-                    c.to_lowercase().to_string()
-                } else {
-                    c.to_uppercase().to_string()
-                }
-            })
-            .collect::<String>(),
-        "leet" => text
-            .chars()
-            .map(|c| match c{
-                'a' | 'A' => '4',
-                'e' | 'E' => '3',
-                'i' | 'I' => '2',
-                'o' | 'O' => '1',
-                'u' | 'U' => '0',
-                's' | 'S' => '5',
-                't' | 'T' => '7',
-                    _ => c,
-            })
-            .collect::<String>(),
-        "no spaces" => text 
-            .chars()
-            .filter(|c| !c.is_whitespace())
-            .collect::<String>(),
-        "acronym" => text 
-            .split_whitespace()
-            .map(|word| word.chars().next().unwrap())
-            .collect::<String>()
-            .to_uppercase(),
-        &_ => todo!(),
-    };
-    println!("{res}")
+    let i = 32; let y = 43;
+    println!("{}",max_ref(&i,&y));
 }
