0ca26f192b2708a722b15abb21634b138b1b502a
n0sync <n0sync.76@gmail.com>
2026-01-30 04:17:11 +0530

    " -- calculating length of a str 🤓"

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
-struct MyStruct<'a> {
-    remainder: Option<&'a str>, //  stores a pointer to  a string: we'll need mutable reference to shrink it.
-}
-
-impl<'a> MyStruct<'a> {
-    fn first_char(&mut self) -> Option<&str> {
-        let remainder = &mut self.remainder?; // gets a mutable reference from Option, avoiding move.
-        let c = &remainder[0..1]; // to read an empty string the program will panic might use remainder.is_empty()
-        if remainder.len() != 1 {
-            // if one than more char shrinks the slice
-            *remainder = &remainder[1..];
-            Some(c)
-        } else {
-            self.remainder.take()
-        }
-    }
-}
-
 fn main() {
-    // test run
-    let mut broken = MyStruct {
-        remainder: Some("Hello"),
-    };
+    let code = "hellowoody";
+    let len = calculate_len(code);
+    println!("{len}");
+}
 
-    for _ in 0..5 {
-        println!("{:?}", broken.first_char());
-    }
+fn calculate_len(s: &str) -> usize{
+    s.len()
 }
