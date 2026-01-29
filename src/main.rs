6eeb105d46528a3331866efa4653bc376ef5605d
n0sync <n0sync.76@gmail.com>
2026-01-30 03:08:36 +0530

    -- string consumer explained

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 struct MyStruct<'a> {
-    remainder: Option<&'a str>,
+    remainder: Option<&'a str>, //  stores a pointer to  a string: we'll need mutable reference to shrink it.
 }
 
 impl<'a> MyStruct<'a> {
     fn first_char(&mut self) -> Option<&str> {
-        let remainder = &mut self.remainder?;
-        let c = &remainder[0..1];
+        let remainder = &mut self.remainder?; // gets a mutable reference from Option, avoiding move.
+        let c = &remainder[0..1]; // to read an empty string the program will panic might use remainder.is_empty()
         if remainder.len() != 1 {
+            // if one than more char shrinks the slice
             *remainder = &remainder[1..];
             Some(c)
         } else {
@@ -16,6 +17,7 @@ impl<'a> MyStruct<'a> {
 }
 
 fn main() {
+    // test run
     let mut broken = MyStruct {
         remainder: Some("Hello"),
     };
