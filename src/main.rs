d9af39dbf8bdc8d6e0b1e8196160602f3192b9f8
n0sync <n0sync.76@gmail.com>
2026-02-05 01:53:51 +0530

    -- module

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,35 @@
-#![allow(unused)]
+mod my_module {
+    pub mod child_module {
+        pub mod grand_child_module {
+            pub(super) fn public_in_grandchild() {  // visibility to parent; my_module
+                println!("public in granchild");
+            }
 
-use crate::outer::public_in_super_func;
-
-pub fn public_func() {
-    // accessable from everywhere
-    println!("Public func");
-}
-
-fn private_func() {
-    // private func i.e for current file only
-    println!("Private func");
-}
-
-pub(crate) fn public_in_crate_func() {
-    // public to whole project;
-    println!("public func from crate");
-}
-
-pub(self) fn public_in_self_func() {
-    // explicit private func;
-    println!("public func from self");
-}
-mod outer {
-    pub(super) fn public_in_super_func() {
-        /* visible to 'outer' and its parent
-        i.e in this case its main.rs file as mod outer is defined inside main.rs */
-        println!("public func from super");
+            pub fn public_for_everyone(){   // for everyone;
+                println!("public for everyone");
+                public_in_self();
+                public_in_grandchild();
+                public_in_module();
+            }
+            pub(self) fn public_in_self() { // to current module only; grand_child_module
+                println!("public in self");
+            }
+            pub(in crate::my_module) fn public_in_module() {    //inside my_module everyone can access it.
+                println!("public from this module");
+            }
+        }
+    }
+    pub mod other_child {
+        pub(super) fn public_in_my_module() { // visibility to parent; my_module
+            println!("accessible from my module");
+            // super::child_module::grand_child_module::public_in_module(); --> can access it;
+        }
     }
 }
 
 fn main() {
-    public_func();
-    private_func();
-    public_in_crate_func();
-    public_in_super_func();
+    use my_module::child_module::grand_child_module;
+    // use my_module::other_child;
+    // other_child::public_in_my_module(); --> this is private under other_child;
+    grand_child_module::public_for_everyone();
 }
