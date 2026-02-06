02ac528b281b8fbf1f1b24806de35e886ef85611
reumer <n0sync.76@gmail.com>
2026-02-07 01:37:34 +0530

    -- traits

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,58 @@
-trait Quack {
-    fn quack(&self);
-}
-struct Duck;
+// https://doc.rust-lang.org/std/mem/union.MaybeUninit.html --> in this union are useful.
 
-impl Quack for Duck {
-    fn quack(&self) {
-        println!("quack");
+struct Sheep {naked: bool, name: &'static str}
+
+trait Animal {
+    fn new(name: &'static str)  -> Self;
+    fn name(&self)  -> &'static str;
+    fn noise(&self)  -> &'static str;
+
+    fn talk(&self){
+        println!("{} says {}",self.name(),self.noise());
     }
 }
-struct Human;
 
-impl Human {
-    fn quack(&self){
-        println!("Humans do not quack");
+impl Sheep {
+    fn is_naked(&self) -> bool {
+        self.naked
+    }
+
+    fn shear(&mut self){
+        if self.is_naked(){
+            println!("{} is already naked",self.name());
+        } else {
+            println!("{} gets a haircut",self.name);
+            self.naked = true;
+        }
+    }
+}
+
+impl Animal for Sheep {
+    fn new(name : &'static str) -> Sheep {
+        Sheep {name : name, naked: false}
+    }
+
+    fn name(&self) -> &'static str{
+        self.name
+    }
+
+    fn noise(&self) -> &'static str{
+        if self.naked {
+            "Baaaaaaaaaaahhhhhhhh"
+        } else {
+            "bahhhhhh!"
+        }
+    }
+
+    fn talk(&self){
+        println!("{} pauses briefly......{}",self.name,self.noise());
     }
 }
 
 fn main() {
-    Duck.quack();
-    Human.quack();
+        let mut dolly:Sheep = Animal::new("Dolly");
+
+        dolly.talk();
+        dolly.shear();
+        dolly.talk();
 }
