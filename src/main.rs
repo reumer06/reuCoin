71a440049898eb79b1fe12e8786db0397f8081f1
reumer06 <n0sync.76@gmail.com>
2026-02-08 01:12:46 +0530

    -- Monomorphization & Static dispatch

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,51 @@
-// Uninitialized var in rust
-// more about this : https://doc.rust-lang.org/std/mem/union.MaybeUninit.html
+// static dispatch
 
-use std::mem::{self, MaybeUninit};
+// https://rustc-dev-guide.rust-lang.org/backend/monomorph.html             -> fast runtime at cost of compile time (creating copies of each will take time)
+
+fn peach<T>() {
+    // monomorphization collector;
+    println!("monomorphization for type: {}", std::any::type_name::<T>());
+}
+
+fn banana() {
+    peach::<u64>();
+    peach::<u32>();
+}
+
+struct Duck {
+    // fields to store data of duck;
+    name: String,
+}
+trait Quack {
+    // shares behaviour that types must implement;
+    fn quack(&self);
+}
+impl Duck {
+    // constructor of Duck struct;
+    fn new(name: String) -> Self {
+        Self { name }
+    }
+}
+
+impl Quack for Duck {
+    // implementation of quack trait for duck;
+    fn quack(&self) {
+        println!("{} wants to say Quackkkkkkkkkkkkkkkkkkkkk", self.name)
+    }
+}
+
+// Monomorphization
+fn duck_say<T: Quack>(quacker: T) {
+    // duck_say uses a "Trait Bound" <T: Quack>
+    // This tells the compiler to accept any type T that implements the Quack trait.
+    quacker.quack();
+}
 
 fn main() {
-    let x: i32 = unsafe { mem::uninitialized() };
-    let y: i32 = unsafe { MaybeUninit::uninit().assume_init() };
+    let duck = Duck::new("Quill".to_string()); // object for trait;
+    duck_say(duck);
 
-    let mut m = MaybeUninit::<&i32>::uninit();
-    m.write(&0);
-    let m = unsafe { m.assume_init() };
+    // duck.quack();           // this won't compile duck has been moved above;
 
-    unsafe fn make_vector(out: *mut Vec<i32>){
-        unsafe {out.write(vec![1,2,3])};
-    }
-
-    let mut v = MaybeUninit::uninit();
-    unsafe {make_vector(v.as_mut_ptr());}
-
-    let v  = unsafe { v.assume_init()};
-    assert_eq!(&v,&[1,2,3]);
-
-    println!("Vector: {:?}",v);
-    println!("{x} and {y} and {m}");
+    banana();
 }
