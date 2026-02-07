d544023708a9201f9788183cecb46bc7a8b7a60e
reumer06 <n0sync.76@gmail.com>
2026-02-08 01:44:13 +0530

    -- Why this won't compile

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,6 @@
-// static dispatch
-
-// https://rustc-dev-guide.rust-lang.org/backend/monomorph.html             -> fast runtime at cost of compile time (creating copies of each will take time)
-
-fn peach<T>() {
-    // monomorphization collector;
-    println!("monomorphization for type: {}", std::any::type_name::<T>());
-}
-
-fn banana() {
-    peach::<u64>();
-    peach::<u32>();
-}
-
-struct Duck {
-    // fields to store data of duck;
-    name: String,
-}
-trait Quack {
-    // shares behaviour that types must implement;
-    fn quack(&self);
-}
-impl Duck {
-    // constructor of Duck struct;
-    fn new(name: String) -> Self {
-        Self { name }
-    }
-}
-
-impl Quack for Duck {
-    // implementation of quack trait for duck;
-    fn quack(&self) {
-        println!("{} wants to say Quackkkkkkkkkkkkkkkkkkkkk", self.name)
-    }
-}
-
-// Monomorphization
-fn duck_say<T: Quack>(quacker: T) {
-    // duck_say uses a "Trait Bound" <T: Quack>
-    // This tells the compiler to accept any type T that implements the Quack trait.
-    quacker.quack();
-}
-
+fn no_param<T>(_: &T) {} /* this won't compile ?Sized mentioned cause even generic parameters without any written trait have hidden trait bound which is
+                          T Sized the type sized is known at compile time we have to use T: ?Sized for writing this unique trait.*/
 fn main() {
-    let duck = Duck::new("Quill".to_string()); // object for trait;
-    duck_say(duck);
-
-    // duck.quack();           // this won't compile duck has been moved above;
-
-    banana();
+    let my_str = "Banana";
+    no_param(my_str);       // ERROR: doesn't have a size known at compile-time
 }
