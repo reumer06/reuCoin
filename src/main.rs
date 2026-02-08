c09c42c37792ee02230d2f2186b5abd1b75523f0
reumer06 <n0sync.76@gmail.com>
2026-02-09 03:38:11 +0530

    -- Generic Param Bounds

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
-fn no_param<T>(_: &T) {} /* this won't compile ?Sized mentioned cause even generic parameters without any written trait have hidden trait bound which is
-                          T Sized the type sized is known at compile time we have to use T: ?Sized for writing this unique trait.*/
-fn main() {
-    let my_str = "Banana";
-    no_param(my_str);       // ERROR: doesn't have a size known at compile-time
+use std::fs::File;
+use std::io::Write;     // --> required for writeln!
+use std::path::Path;    // --> Require for AsRef<Path>
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+
+trait Saveable: Display {       // -> Trait with Supertrait; i.e Saveable only be implemented if you implement Display
+    fn save<P>(&self, path: P) -> std::io::Result<()>
+    where
+        P: AsRef<Path>,     // --> P: AsRef<Path> allow to pass &str, String or Path objects
+    {
+        // ? is there for error handling it will stop the entire function when something fails;
+        let mut file = File::create(path.as_ref())?;     // --> path.as_ref() converts generic P into a standard &Path;
+        writeln!(file, "{}", self.to_string())?;                   // --> converts to String because saveable requires Display;
+
+        Ok(())
+    }
+}
+
+struct User {
+    name: String,
+    id: u32,
+}
+// Without this the compiler will refuse to implement Saveable;
+impl Display for User {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "User: {} (ID: {})", self.name, self.id)
+    }
+}
+
+impl Saveable for User {}       // implement our Trait;
+fn main() -> std::io::Result<()> {      // -->  for io errors
+    let first_user = User {
+        name: String::from("littlejames"),
+        id: 42,
+    };
+
+    first_user.save("user_data.txt")?;
+
+    println!("User info saved successful");
+    Ok(())          // returns okay if everything went well;
 }
