b5602324cec3eafcac57edaea58ce06a00658501
reumer06 <n0sync.76@gmail.com>
2026-02-18 16:59:55 +0530

    -- existential type

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,24 @@
-use std::fmt::Debug;
-trait ConvertTo<T: Debug> {
-    // trait can have generic parameters which then can have trait bounds,
-    fn print_a_t(t: T) {
-        println!("{:?}", t);
-    }
-    fn convert(&self) -> T;
-}
+// Existential type.
 
-trait Printable: std::fmt::Display {
-    // super trait.
-    fn print(&self) {
-        println!("{}", self);
-    }
-}
-
-// trait can have their own associated types and const;
-trait Vehicle {
-    type Energy;
-    const WHEELS: u8;
-
-    fn energy_source(&self) -> Self::Energy;
-
-    fn print_wheels() {
-        println!("the vehicle has {} wheels", Self::WHEELS); // when const you use Self instead of self;
-    }
-}
-
-struct FixedArray<T, const N: usize> {
-    data: [T; N],
-}
-
-trait ArrayOps<T, const N: usize> {
-    fn first(&self) -> Option<&T>;
-    fn last(&self) -> Option<&T>;
-
-    fn size(&self) -> usize {
-        // cannot return self.size() it would be recursive call;
-        N
-    }
-}
-
-impl<T, const N: usize> ArrayOps<T, N> for [T; N] {
-    fn first(&self) -> Option<&T> {
-        if N > 0 {
-            Some(&self[0])
+fn iter() -> impl Iterator<Item = i32> {
+    // RPIT  (Return Position Impl type);
+    let mut i = 0;
+    std::iter::from_fn(move || {
+        i += 1;
+        if true {
+            Some(i)
         } else {
             None
         }
-    }
-    fn last(&self) -> Option<&T> {
-        if N > 0 {
-            Some(&self[N - 1])
-        } else {
-            None
-        }
-    }
-}
-struct Data {
-    age: usize,
+    })
 }
 
+//
+// fn iter2() -> i32 {
+//     let mut i = 0;
+// }
 fn main() {
-    let person = Data { age: 25 };
-
-    let list = [person];
-    println!("array size is {}", list.size());
-
-    if let Some(item) = list.first() {
-        println!("the person age is {}", item.age);
+    for val in iter() {
+        println!("the value is {}", val);
     }
 }
