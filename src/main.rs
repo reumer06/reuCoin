a7500c8463b9479a4ff8ae93ae63dc1a94475aae
reumer06 <n0sync.76@gmail.com>
2026-02-18 17:41:49 +0530

    -- impl APIT

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-fn print_type1<T: std::fmt::Debug>(u : T){
-    println!("{:?}",u);
-}
-fn print_type2(u : impl std::fmt::Debug){
+fn print1(u : impl std::fmt::Debug){
     println!("{:?}",u);
 }
 
 fn main() {
-    let x = (0..25)
-        .map(|x| x * 2)
-        .filter(|x| x % 3  == 0)
-        .collect();
-    print_type1::<Vec<i32>>(x);
-    print_type2::<Vec<i32>>(x); // 0 generic parameters.
+    let x = (0..10)
+        .map(|x|x * 10)
+        .filter(|x| x % 5 == 0)
+        .collect::<Vec<i32>>();
+    print1(x);
 }
