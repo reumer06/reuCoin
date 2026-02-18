1e89aae478d7edf6185dd8767e3c84b8c23ad586
reumer06 <n0sync.76@gmail.com>
2026-02-18 17:33:54 +0530

    -- 0 generic parameters

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,15 @@
-
-
-fn iter1(iterator: impl Iterator<Item = i32>) { // cannot implement turbofish here.
-    // APIT (Argument position impl trait) // lazy way of writing generics.
-    // same as the generic type but generic parameter is anonymous; generic type can only be inferred.
-    for i in iterator {
-        println!("value: {} ", i);
-    }
+fn print_type1<T: std::fmt::Debug>(u : T){
+    println!("{:?}",u);
 }
-
-fn iter2<I: Iterator<Item = i32>>(iterator: I) {
-    // inline generic
-    for i in iterator {
-        println!("value: {} ", i);
-    }
-}
-
-fn iter3<I>(iterator: I)    // where clause
-where
-    I: Iterator<Item = i32>,
-{
-    for i in iterator {
-        println!("value: {} ", i);
-    }
+fn print_type2(u : impl std::fmt::Debug){
+    println!("{:?}",u);
 }
 
 fn main() {
-    // turbofish ::<>
-    let numbers = (1..5).collect::<Vec<i32>>();
-    let my_vec : Vec<i32> = vec![1,2,3,1];
-    iter2::<std::vec::IntoIter<i32>>(my_vec.into_iter());       // cannot do same for iter1 no generic arguments found;
+    let x = (0..25)
+        .map(|x| x * 2)
+        .filter(|x| x % 3  == 0)
+        .collect();
+    print_type1::<Vec<i32>>(x);
+    print_type2::<Vec<i32>>(x); // 0 generic parameters.
 }
