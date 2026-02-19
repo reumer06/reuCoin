d454143fb1c2bd43a4acd2940f37db1c9cba936c
reumer06 <n0sync.76@gmail.com>
2026-02-20 00:26:02 +0530

    -- currying & partial application

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,23 @@
-enum Message {
-    Quit,                       // plain name
-    Move { x: i32, y: i32 },    // struct like
-    Write(String),              // tuple like
-    ChangeColor(i32, i32, i32), // tuple like
+fn hi() {
+    println!("hi");
+}
+
+fn add(i: i32, j: i32) -> i32 {
+    i + j
+}
+
+fn c_add(a: i32) -> impl Fn(i32) -> i32 {
+    move |b| a + b
 }
 
 fn main() {
-    let some_message = Message::Write(String::from("quill!"));
+    let add_five = |x| add(5, x); // partial application { half filled arguments};
+    println!("partial application: {}", add_five(10));
 
-    if let Message::Write(ref some_text) = some_message {
-        // conditional peak
-        println!("{some_text} was written!");
-    }
+    let _result = c_add(5)(10); // currying {breaking the func into taking multiple args ;
+    let add_ten = c_add(10);
+    println!("currying: {}", add_ten(10));
 
-    if matches!(some_message, Message::Write(_)) {
-        // conditional check
-        println!("bill");
-    }
-
-    let Message::Write(ref some_text) = some_message else {
-        // guard clause
-        return;
-    };
-
-    let some_text: String = if let Message::Write(ref text) = some_message {
-        // transformation
-        text.clone()
-    } else {
-        unreachable!()
-    };
-
-    // println!("{some_text}");
-    match some_message {
-        Message::Quit => println!("Quit"),
-        Message::Move { x, y } => println!("values of x and y are {x} and {y}"),
-        Message::Write(text) => println!("Text message: {text}"),
-        Message::ChangeColor(r, g, b) => println!("change the rgb values to: {r}, {g} , {b}"),
-    }
+    let u = hi;
+    u();
 }
