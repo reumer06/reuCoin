d38a75f1fafdb44c005ae702ca419d46b7446596
reumer06 <n0sync.76@gmail.com>
2026-02-19 23:53:38 +0530

    -- enums

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
-union MyUnion {
-    p: u32,
-    q: f32,
+enum Message {
+    Quit,                       // plain name
+    Move { x: i32, y: i32 },    // struct like
+    Write(String),              // tuple like
+    ChangeColor(i32, i32, i32), // tuple like
 }
+
 fn main() {
-    let mut my_union = MyUnion { p: 10 };
-    my_union.p = 12334553; // writing to union is safe;
-    unsafe {
-        // reading from union is unsafe;
-        // type punning writing the data as one type and reading it with another; [very unsafe]
-        println!("p : {}", my_union.p);
-        println!("q : {}", my_union.q); // bunch of zeros.
+    let some_message = Message::Write(String::from("quill!"));
+
+    if let Message::Write(ref some_text) = some_message {
+        // conditional peak
+        println!("{some_text} was written!");
+    }
+
+    if matches!(some_message, Message::Write(_)) {
+        // conditional check
+        println!("bill");
+    }
+
+    let Message::Write(ref some_text) = some_message else {
+        // guard clause
+        return;
+    };
+
+    let some_text: String = if let Message::Write(ref text) = some_message {
+        // transformation
+        text.clone()
+    } else {
+        unreachable!()
+    };
+
+    // println!("{some_text}");
+    match some_message {
+        Message::Quit => println!("Quit"),
+        Message::Move { x, y } => println!("values of x and y are {x} and {y}"),
+        Message::Write(text) => println!("Text message: {text}"),
+        Message::ChangeColor(r, g, b) => println!("change the rgb values to: {r}, {g} , {b}"),
     }
 }
