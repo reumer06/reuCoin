cf61e2fadcbaa91d2dc3e4809c8608e511aaa04c
reumer06 <n0sync.76@gmail.com>
2026-02-20 22:59:32 +0530

    -- move

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-// Closures
+use std::thread;
 
 fn main() {
-    let x = 42;
-    let u = || println!("{x}");
-    u();
-
-    let z = |x: i32| x * 2; // in most cases type cannot be inferred; specify them directly.
-    println!("{}", z(7));
-
-    let y = |(a, b): (i32, i32)| -> i32 { a + b }; // return type can also be specified.
-    y((10, 23));
+    let numbers = vec![1, 2, 3, 4, 5];
+    let handle = thread::spawn(move || {    // threads will own the numbers vector by the move keyword.
+        let sum: i32 = numbers.iter().sum();
+        println!("the sum is: {}", sum);
+    });
+    // wait from the thread to complete.
+    handle.join().unwrap();
 }
