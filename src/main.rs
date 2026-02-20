615d97b71f35184500a342a0f46fce3c335baadd
reumer06 <n0sync.76@gmail.com>
2026-02-20 23:10:14 +0530

    -- iterators

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::thread;
-
+struct ConstantNumber {
+    number: usize,
+}
+impl Iterator for ConstantNumber {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.number)
+    }
+}
 fn main() {
-    let numbers = vec![1, 2, 3, 4, 5];
-    let handle = thread::spawn(move || {    // threads will own the numbers vector by the move keyword.
-        let sum: i32 = numbers.iter().sum();
-        println!("the sum is: {}", sum);
-    });
-    // wait from the thread to complete.
-    handle.join().unwrap();
+    let constant = ConstantNumber { number: 5 };
+    let numbers: Vec<_> = constant.take(10).collect();
+    println!("{:?}", numbers);
 }
