b6ad360173e56024aa27f62ac359d1e7e6b399e1
reumer06 <n0sync.76@gmail.com>
2026-02-24 02:17:25 +0530

    feat: impl Block

--- src/lib/src/types.rs
+++ src/lib/src/types.rs
@@ -10,6 +10,22 @@ impl Blockchain {
         self.blocks.push(block);
     }
 }
-pub struct Block;
+pub struct Block {
+    pub header: BlockHeader,
+    pub transactions: Vec<Transaction>,
+}
+
+impl Block {
+    pub fn new(header: BlockHeader, transactions: Vec<Transaction>) -> Self {
+        Block {
+            header,
+            transactions,
+        }
+    }
+    pub fn hash(&self) -> ! {
+        unimplemented!() // never type.
+    }
+}
+
 pub struct BlockHeader;
 pub struct Transaction;
