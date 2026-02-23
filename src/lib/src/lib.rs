75d764025a4ab68b38d5db8eff140433874304c8
reumer06 <n0sync.76@gmail.com>
2026-02-24 01:56:47 +0530

    feat: create unit structs

--- src/lib/src/lib.rs
+++ src/lib/src/lib.rs
@@ -1,7 +1,7 @@
 use uint::construct_uint;
 
 construct_uint! {
-    // construct an unsigned 256-bit integer;
+    // construct an unsigned 256-bit integer
     pub struct U256(4);
 }
 pub mod crypto;
---
+++ src/lib/src/types.rs
@@ -0,0 +1,4 @@
+pub struct Blockchain;
+pub struct Block;
+pub struct Blockheader;
+pub struct Transaction;
