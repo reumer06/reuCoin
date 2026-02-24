f2b798c00c75352b9641dc333a668305655d420f
reumer06 <n0sync.76@gmail.com>
2026-02-25 01:19:24 +0530

    feat: import

--- src/lib/src/crypto.rs
+++ src/lib/src/crypto.rs
@@ -1,3 +1,5 @@
-pub struct Signature;
-pub struct PublicKey;
-pub struct PrivateKey;
+use ecdsa::{Signature as EcdsaSignature, SigningKey, VerifyingKey, signature::Signer};
+use k256::Secp256k1;
+pub struct Signature(EcdsaSignature<Secp256k1>);
+pub struct PublicKey(VerifyingKey<Secp256k1>);
+pub struct PrivateKey(SigningKey<Secp256k1>);
