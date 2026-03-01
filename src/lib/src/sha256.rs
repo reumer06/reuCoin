a9c6982ea3502380fc9a38ea92d4ea40dc56814e
reumer06 <n0sync.76@gmail.com>
2026-03-01 14:59:48 +0530

    feat: std::fmt::Display for Hash

--- src/lib/src/sha256.rs
+++ src/lib/src/sha256.rs
@@ -1,11 +1,13 @@
 use crate::U256;
 use serde::Serialize;
 use sha256::digest;
+use std::fmt;
 #[derive(Clone, Copy, Serialize)]
 
 pub struct Hash(U256);
 
 impl Hash {
+    // hash anything that can be serde Serialized via ciborium
     pub fn hash<T: serde::Serialize>(data: &T) -> Self {
         let mut serialized: Vec<u8> = vec![];
         if let Err(e) = ciborium::into_writer(data, &mut serialized) {
@@ -20,10 +22,18 @@ impl Hash {
         let hash_array: [u8; 32] = hash_bytes.as_slice().try_into().unwrap();
         Hash(U256::from(hash_array))
     }
+    // check if a hash matches a target
     pub fn matches_target(&self, target: U256) -> bool {
         self.0 <= target
     }
+    // zero hash;
     pub fn zero() -> Self {
         Hash(U256::zero())
+    }
+}
+
+impl fmt::Display for Hash {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:x}", self.0)
     }
 }
