b8ceb7dc74057f05614c335e3ede1d60f65b0684
reumer06 <n0sync.76@gmail.com>
2026-03-03 02:52:07 +0530

    feat: add custom signkey module

--- src/lib/src/crypto.rs
+++ src/lib/src/crypto.rs
@@ -1,6 +1,36 @@
 use ecdsa::{Signature as ECDSASignature, SigningKey, VerifyingKey};
 
 use k256::Secp256k1;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Signature(ECDSASignature<Secp256k1>);
+#[derive(Debug, Serialize, Deserialize, Clone)]
+
 pub struct PublicKey(VerifyingKey<Secp256k1>);
-pub struct PrivateKey(SigningKey<Secp256k1>);
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct PrivateKey(#[serde(with = "signkey_serde")] pub SigningKey<Secp256k1>);
+mod signkey_serde {
+    use serde::Deserialize;
+
+    pub fn serialize<S>(
+        key: &super::SigningKey<super::Secp256k1>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_bytes(&key.to_bytes())
+    }
+
+    pub fn deserialize<'de, D>(
+        deserializer: D,
+    ) -> Result<super::SigningKey<super::Secp256k1>, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let bytes: Vec<u8> = Vec::<u8>::deserialize(deserializer)?;
+        // Note: Consider replacing .unwrap() with proper error mapping for production
+        Ok(super::SigningKey::from_slice(&bytes).unwrap())
+    }
+}
