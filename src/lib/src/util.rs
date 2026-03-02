a7fb668042280475b9b510a7a081aae57556d187
reumer06 <n0sync.76@gmail.com>
2026-03-02 23:00:24 +0530

    feat: impl MerkleRoot

--- src/lib/src/util.rs
+++ src/lib/src/util.rs
@@ -1,2 +1,22 @@
 use crate::{sha256::Hash, types::Transaction};
-pub struct MerkleRoot;
+pub struct MerkleRoot(Hash);
+
+impl MerkleRoot {
+    pub fn calculate(transactions: &[Transaction]) -> MerkleRoot {
+        let mut layer: Vec<Hash> = vec![];
+        for transaction in transactions {
+            layer.push(Hash::hash(transaction));
+        }
+        while layer.len() > 1 {}
+        {
+            let mut new_layer = vec![];
+            for pair in layer.chunks(2) {
+                let left = pair[0];
+                let right = pair.get(1).unwrap_or(&pair[0]);
+                new_layer.push(Hash::hash(&[left, *right]));
+            }
+            layer = new_layer;
+        }
+        MerkleRoot(layer[0])
+    }
+}
