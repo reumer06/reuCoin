3c7d99576bfd8b011c0271634331e37da5aa7af4
reumer06 <n0sync.76@gmail.com>
2026-03-08 05:07:17 +0530

    -- unwrap_or_default

--- eh/eh.rs
+++ eh/eh.rs
@@ -6,13 +6,7 @@
 fn main() {
     let somevalue = Some("safe to unwarp");
     let nonevalue: Option<&str> = None;
-    println!(
-        "somevalue: {}",
-        somevalue.unwrap_or_else(|| "this will not panic")
-    );
+    println!("somevalue: {}", somevalue.unwrap_or_default());
 
-    println!(
-        "nonevalue: {}",
-        nonevalue.unwrap_or_else(|| { "might panic" })
-    );
+    println!("nonevalue: {}", nonevalue.unwrap_or_default());
 }
