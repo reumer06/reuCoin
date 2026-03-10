572f3b7e3bc359f5e446c96133973b15a8fbb151
reumer06 <n0sync.76@gmail.com>
2026-03-10 05:43:18 +0530

    feat: idiomatic error

--- eh/eh.rs
+++ eh/eh.rs
@@ -1,10 +1,37 @@
-// pub enum Option<T> {     // already defined;
-//     Some(T),
-//     None,
+// pub enum Result<T, E> {
+//     Ok(T),
+//     Err(e),
 // }
 
-fn main() {
-    for i in Some(32) {
-        println!("{i}");
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+enum MyError {
+    Io(std::io::Error),
+    Parse(std::num::ParseIntError),
+    Notfound(String),
+}
+
+impl Error for MyError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            MyError::Io(e) => Some(e),
+            MyError::Parse(e) => Some(e),
+            MyError::Notfound(e) => None,
+        }
+    }
+}
+
+impl From<std::io::Error> for MyError {
+    fn from(error: std::io::Error) -> Self {
+        MyError::Io(error)
+    }
+}
+
+impl From<std::num::ParseIntError> for MyError {
+    fn from(error: std::num::ParseIntError) -> Self {
+        MyError::Parse(error)
     }
 }
