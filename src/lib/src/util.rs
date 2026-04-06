9a738e70397b4523cf76869aacfa9a0a2e1aac32
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-04-07 02:31:27 +0530

    feat: add trait methods

--- src/lib/src/util.rs
+++ src/lib/src/util.rs
@@ -3,9 +3,20 @@ use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::{Read, Result as IoResult, Write};
 use std::path::Path;
-pub trait Saveable {
-    fn load<I: Read>(&self, reader: I);
-    fn save<O: Write>(&self, reader: O);
+pub trait Saveable
+where
+    Self: Sized,
+{
+    fn load<I: Read>(reader: I) -> IoResult<Self>;
+    fn save<O: Write>(&self, reader: O) -> IoResult<()>;
+    fn save_to_file<P: AsRef<Path>>(&self, path: P) -> IoResult<()> {
+        let file = File::create(&path)?;
+        self.save(file)
+    }
+    fn load_from_file<P: AsRef<Path>>(path: P) -> IoResult<Self> {
+        let file = File::open(&path)?;
+        Self::load(file)
+    }
 }
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct MerkleRoot(Hash);
