365c11803b26f3d7565748c49c02e3a333f14c2d
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-04-14 02:55:08 +0530

    feat: parse block path

--- src/miner/src/main.rs
+++ src/miner/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    // parse the block path and steps count from the first and second argument
+    let (path, steps) = if let (Some(arg), Some(arg1)) = (env::args().nth(1), env::args().nth(2)) {
+        (arg, arg1)
+    } else {
+        eprintln!("Usage: miner <block_file> <steps>");
+        exit(1);
+    };
 }
