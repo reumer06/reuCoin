cb7ddec949bb93615930df4f89f970888dbe8d8c
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-04-24 00:54:51 +0530

    feat: implement async connection

--- src/miner/src/main.rs
+++ src/miner/src/main.rs
@@ -1,31 +1,41 @@
+use lib::crypto::PublicKey;
+use lib::network::Message;
 use lib::types::Block;
 use lib::util::Saveable;
 use std::env;
 use std::process::exit;
+use tokio::net::TcpStream;
 
-fn main() {
-    // parse the block path and steps count from the first and second argument
-    let (path, steps) = if let (Some(arg), Some(arg1)) = (env::args().nth(1), env::args().nth(2)) {
-        (arg, arg1)
-    } else {
-        eprintln!("Usage: miner <block_file> <steps>");
+fn usage() -> ! {
+    eprintln!(
+        "Usage: {} <address> <public_key_file>",
+        env::args().next().unwrap()
+    );
+    exit(1);
+}
+
+#[tokio::main]
+async fn main() {
+    let address = match env::args().nth(1) {
+        Some(address) => address,
+        None => usage(),
+    };
+
+    let public_key_file = match env::args().nth(2) {
+        Some(public_key_file) => public_key_file,
+        None => usage(),
+    };
+
+    let Ok(PublicKey) = Public::load_from_file(&public_key_file) else {
+        eprintln!("Error reading public key from file {} ", public_key_file);
         exit(1);
     };
-    // parse steps count
-    let steps: usize = if let Ok(s @ 1..=usize::MAX) = steps.parse() {
-        s
-    } else {
-        eprintln!("<steps> should be a positive integer");
-        exit(1);
+    let println!("Connecting to {address} to mine with {public_key:?}");
+    let mut stream = match TcpStream::connect(&address).await {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("Failed to connect to {}: {}", address, e);
+            exit(1);
+        }
     };
-    let og_block = Block::load_from_file(path).expect("Failed to load block");
-    let mut block = og_block.clone();
-
-    while !block.header.mine(steps) {
-        println!("mining...");
-    }
-    println!("original:{:#?}", og_block);
-    println!("hash: {}", og_block.header.hash());
-    println!("final: {:#?}", block);
-    println!("hash: {}", block.header.hash());
 }
