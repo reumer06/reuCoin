cabbb8ef3fc028962e4b8bf44e7071a739caa823
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-04-25 19:48:56 +0530

    feat: add miner struct

--- src/miner/src/main.rs
+++ src/miner/src/main.rs
@@ -23,39 +23,46 @@ struct Cli {
     #[arg(short, long)]
     public_key_file: String,
 }
-
-fn usage() -> ! {
-    eprintln!(
-        "Usage: {} <address> <public_key_file>",
-        env::args().next().unwrap()
-    );
-    exit(1);
+struct Miner {
+    public_key: PublicKey,
+    stream: Mutex<TcpStream>,
+    current_template: Arc<std::sync::Mutex<Option<Block>>>,
+    mining: Arc<AtomicBool>,
+    mined_block_sender: flume::Sender<Block>,
+    miner_block_receiver: flume::Receiver<Block>,
 }
+// fn usage() -> ! {
+//     eprintln!(
+//         "Usage: {} <address> <public_key_file>",
+//         env::args().next().unwrap()
+//     );
+//     exit(1);
+// }
 
-#[tokio::main]
-async fn main() {
-    let address = match env::args().nth(1) {
-        Some(address) => address,
-        None => usage(),
-    };
-
-    let public_key_file = match env::args().nth(2) {
-        Some(public_key_file) => public_key_file,
-        None => usage(),
-    };
-
-    let Ok(PublicKey) = PublicKey::load_from_file(&public_key_file) else {
-        eprintln!("Error reading public key from file {} ", public_key_file);
-        exit(1);
-    };
-    println!("Connecting to {address} to mine with {PublicKey:?}");
-    let mut stream = match TcpStream::connect(&address).await {
-        Ok(stream) => stream,
-        Err(e) => {
-            eprintln!("Failed to connect to {}: {}", address, e);
-            exit(1);
-        }
-    };
-    let message = Message::FetchTemplate(PublicKey);
-    message.send(&mut stream);
-}
+// #[tokio::main]
+// async fn main() {
+//     let address = match env::args().nth(1) {
+//         Some(address) => address,
+//         None => usage(),
+//     };
+//
+//     let public_key_file = match env::args().nth(2) {
+//         Some(public_key_file) => public_key_file,
+//         None => usage(),
+//     };
+//
+//     let Ok(PublicKey) = PublicKey::load_from_file(&public_key_file) else {
+//         eprintln!("Error reading public key from file {} ", public_key_file);
+//         exit(1);
+//     };
+//     println!("Connecting to {address} to mine with {PublicKey:?}");
+//     let mut stream = match TcpStream::connect(&address).await {
+//         Ok(stream) => stream,
+//         Err(e) => {
+//             eprintln!("Failed to connect to {}: {}", address, e);
+//             exit(1);
+//         }
+//     };
+//     let message = Message::FetchTemplate(PublicKey);
+//     message.send(&mut stream);
+// }
