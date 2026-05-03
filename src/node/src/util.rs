1c025d7db95f5b912ae478e6e6d0dd46ed04fdb8
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-05-03 15:42:08 +0530

    init

---
+++ src/node/src/util.rs
@@ -0,0 +1,8 @@
+use anyhow::{Context, Result};
+use lib::network::Message;
+use lib::types::Blockchain;
+use lib::util::Saveable;
+use tokio::net::TcpStream;
+use tokio::time;
+
+pub async fn load_blockchain() {}
