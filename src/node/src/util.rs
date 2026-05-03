942d580f8c5a32dbeaf2454f781a35f24c72edf5
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-05-03 15:49:19 +0530

    feat(util): implement load blockchain

--- src/node/src/util.rs
+++ src/node/src/util.rs
@@ -5,4 +5,19 @@ use lib::util::Saveable;
 use tokio::net::TcpStream;
 use tokio::time;
 
-pub async fn load_blockchain() {}
+pub async fn load_blockchain(blockchain_file: &str) -> Result<()> {
+    println!("blockchain exits, file loading...");
+    let new_blockchain = Blockchain::load_from_file(blockchain_file)?;
+    println!("Blockchain loaded");
+    let mut blockchain = crate::BLOCKCHAIN.write().await?;
+    *blockchain = new_blockchain;
+    println!("rebuilding utxos...");
+    blockchain.rebuild_utxos();
+    println!("utxos rebuilt");
+    println!("checking if target needs to be adjusted...");
+    println!("current target : {}", blockchain.target());
+    blockchain.try_adjust_target();
+    println!("new target: {}", blockchain.target());
+    println!("initialization complete");
+    Ok(())
+}
