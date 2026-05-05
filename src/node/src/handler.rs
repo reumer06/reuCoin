a68ccd947e347387b2f6792095ee6254ee9cb035
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-05-06 03:22:40 +0530

    feat: read message from socket

--- src/node/src/handler.rs
+++ src/node/src/handler.rs
@@ -4,6 +4,18 @@ use lib::sha256::Hash;
 use lib::types::{Block, BlockHeader, Transaction, TransactionOutput};
 use lib::util::MerkleRoot;
 use tokio::net::TcpStream;
+// use tokio::net::windows::named_pipe::PipeMode::Message;
 use uuid::Uuid;
 
-pub async fn handle_connection(mut socket: TcpStream) {}
+pub async fn handle_connection(mut socket: TcpStream) {
+    loop {
+        // read a message from the socket
+        let message = match Message::receive_async(&mut socket).await {
+            Ok(message) => message,
+            Err(e) => {
+                println!("invalid message from peer:{e}, closing that connection");
+                return;
+            }
+        };
+    }
+}
