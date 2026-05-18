3c34513a07b1dda2f17698049ad2832d4e265c38
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-05-18 15:26:01 +0530

    feat: add ClI argument handling

--- src/wallet/src/main.rs
+++ src/wallet/src/main.rs
@@ -1,5 +1,30 @@
 mod core;
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use kanal::bounded;
+use lib::types::Transaction;
+use std::io::{self, Write};
+use std::path::PathBuf;
+use tokio::time::{self, Duration};
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+    #[arg(short, long, value_name = "ADDRESS")]
+    node: Option<String>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    GenerateConfig {
+        #[arg(short, long, value_name = "FILE")]
+        output: PathBuf,
+    },
+}
 fn main() {
     println!("Hello, world!");
 }
